c0df08fb1c95b96b89b4af1f27486cdee83fbdf2
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-04-04 01:05:36 -0400

    Completed Hero Section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head';
 import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {AiFillInstagram} from 'react-icons/ai'
+import Image from "next/image"
+import pete from "../public/pete.png"
+
 
 export default function Home() {
   return (
@@ -15,7 +19,7 @@ export default function Home() {
       <main className="bg-white px-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className='text-xl'>Pete Estates</h1>
+            <h1 className='text-xl font-burtons'>Pete Estates</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
@@ -25,6 +29,17 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+          <div className='text-center p-10 py-10'>
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">Petrus Francois</h2>
+            <h3 className='text-2xl py-2'>Real Estate Investor</h3>
+            <p className='text-md py-5 leading-8 text-gray-800'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tincidunt ipsum a lectus auctor suscipit. Sed consequat nisi quis dapibus tincidunt. Donec vel finibus turpis.</p>
+          </div>
+          <div className="text-5xl flex justify-center text-grey-600">
+            <AiFillInstagram/>
+          </div>
+          <div className="relative mx-auto rounded-full w-80 h-80 mt-20 ">
+            <Image src={pete}/>
+          </div>
         </section>
       </main>
     </div>
